ee57c591eac0a6f210c49a059103cc123a4ec3c1
Elise <e260299@dac.unicamp.br>
2026-01-12 16:03:57 -0300

    correct formatting wip

--- scrapper_dac_2026_wip.py
+++ scrapper_dac_2026_wip.py
@@ -12,15 +12,30 @@ class Disciplina:
     name: str
 
 @dataclass
+class Lingua:
+    title: str
+    data: list[Disciplina]
+
+@dataclass
+class Eletiva:
+    title: str
+    cred: str
+    data: list[Disciplina]
+
+@dataclass
 class Modalidade:
     codigo: str
     title: str
     data: list[Disciplina]
+    eletivas: list[Eletiva]
 
 @dataclass
 class Curso:
     numero: int
     name: str
+    maxcred: int
+    obrigarorias: list[Disciplina]
+    lingua: list [Lingua]
     modalidades: list[Modalidade]
 
 class Scraper:
@@ -34,11 +49,8 @@ class Scraper:
         time.sleep(self.delay)
         return BeautifulSoup(response.content, 'html.parser')
     
-    def parse_modalidades(self, key: str, url: str) -> dict[str, list[Tag]]:
-        soup = self.get_soup(url)
-        div = soup.find('div', class_ = "small-12 columns pad-content professores")
-
-        rows = div.find_all(key)
+    def parse_by_key(self, key: str, soup) -> dict[str, list[Tag]]:
+        rows = soup.find_all(key)
         parsed = {}
 
         for row in rows:
@@ -70,29 +82,63 @@ class Scraper:
 
         return list_disciplinas
 
-    def scrape_curso(self) -> list[Curso]:
+    def scrape_modalidade(self, soup) -> list[Modalidade]: #todo - separate eletetivas and obrigatorias
+        modalidades_list = []
+        eletivas_list = []
+        for title, parsed in soup.items():
+            disciplinas_list = self.scrape_disciplina(parsed)
+            split_title = title.split(" - ")
+            modalidade_new = Modalidade(split_title[0], split_title[1], disciplinas_list, eletivas_list)
+            modalidades_list.append(modalidade_new)
+        
+        return modalidades_list
+    
+    def scrape_curso(self) -> list[Curso]: #todo - convert dict to soup-like object
         cursos_list = []
         soup = self.get_soup()
 
         rows = soup.find_all('li', class_ = "accordion-navigation")
         for row in rows:
+
             curso_heading = (row.find('a', class_ = "rotulo-curso").text).split(" - ")
             curso_code = int(curso_heading[0])
             curso_name = curso_heading[1]
 
+            curso_maxcred = 0 #placeholder
+
             link = row.find('a', string = "Currículo Pleno").get('href')
             next_url = urljoin(self.main_url, link)
+            curriculo_soup = self.get_soup(next_url)
+            div = curriculo_soup.find('div', class_ = "small-12 columns pad-content professores")
+            
+            h2_dict = self.parse_by_key('h2', div)
+            first_h2_key = list(h2_dict.keys())[0]
+            curso_header_content = h2_dict.pop(first_h2_key)
 
-            modalidades = self.parse_modalidades('h2', next_url)
+            curso_modalidades = []
+            if h2_dict:
+                curso_modalidades = self.scrape_modalidade(h2_dict)
 
-            modalidades_list = []
-            for title, parsed in modalidades.items():
-                disciplinas_list = self.scrape_disciplina(parsed)
-                split_title = title.split(" - ")
-                modalidade_new = Modalidade(split_title[0], split_title[1], disciplinas_list)
-                modalidades_list.append(modalidade_new)
-
-            curso_new = Curso(curso_code, curso_name, modalidades_list)
+            header_soup = BeautifulSoup('', 'html.parser')
+            for elem in curso_header_content:
+                header_soup.append(elem)
+            
+            h3_dict = self.parse_by_key('h3', header_soup)
+            
+            first_h3_value = list(h3_dict.values())[0]
+            curso_obrigatorias = self.scrape_disciplina(first_h3_value)
+        
+            curso_linguas = 'placeholder'
+            
+            curso_new = Curso(
+                curso_code,
+                curso_name,
+                curso_maxcred,
+                curso_obrigatorias,
+                curso_linguas,
+                curso_modalidades
+            )
+            
             cursos_list.append(curso_new)
             time.sleep(self.delay)
             print(f"scraped {curso_new} : CURSO")
